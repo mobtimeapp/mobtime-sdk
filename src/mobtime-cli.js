7564414152932861301b5b3a2d73ecc068673173
Alex Barry <alex.barry@gmail.com>
2020-09-08 20:33:25 +0000

    WIP SDK

--- src/mobtime-cli.js
+++ src/mobtime-cli.js
@@ -4,15 +4,45 @@ import WebSocket from 'ws';
 console.log('mobtime-sdk', process.argv);
 
 const timerId = process.argv[process.argv.length - 1];
-const mobtimeUrl = `wss://dev.mobti.me/${timerId}`;
 
-const socket = new WebSocket(mobtimeUrl);
+class Mobtime {
+  constructor(mobtimeUrl) {
+    const socket = new WebSocket(mobtimeUrl);
 
-socket.on('open', () => {
-  console.log(`Connected to ${mobtimeUrl}`);
-  socket.send(JSON.stringify({type:'client:new'}));
-});
+    this.callbacks = {
+      'timer:ownership': [],
+      'mob:update': [],
+      'goals:update': [],
+      'settings:update': [],
+    };
 
-socket.on('message', (data) => {
-  console.log('> ', data);
+    socket.on('open', () => {
+      console.log(`Connected to ${mobtimeUrl}`);
+      socket.send(JSON.stringify({type:'client:new'}));
+    });
+
+    socket.on('message', (data) => {
+      const message = JSON.parse(data);
+      console.log('> ', message);
+      for(const callback of (this.callbacks[message.type] || [])) {
+        callback();
+      }
+    });
+  }
+  
+  addEventListener(type, callback){
+    if (!(type in this.callbacks)) {
+      return false;
+    }
+
+    this.callbacks[type].push(callback);
+
+    return true;
+  }
+
+}
+
+const timer = new Mobtime(`wss://dev.mobti.me/${timerId}`);
+timer.addEventListener('timer:ownership', () => {
+  console.log('!!!!!! someone set the ownership !!!!!!');
 });
